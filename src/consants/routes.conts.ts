b650fde02f982f424d87c263cbfada8f18f23aa0
Dmitrii Ivanov <dmi3iva@gmail.com>
2026-04-14 21:15:35 +0300

    fixed routes

--- src/consants/routes.conts.ts
+++ src/consants/routes.conts.ts
@@ -1,5 +1,5 @@
 export const ROUTES = {
-  blogs: "/ht_02/api/blogs",
-  posts: "/ht_02/api/posts",
-  testings: "/ht_02/api/testing/all-data",
+  blogs: "/blogs",
+  posts: "/posts",
+  testings: "/testing/all-data",
 };
