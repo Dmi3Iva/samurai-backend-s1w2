191bf81b38a512008e2d37c6020701168cbfc3a8
Dmitrii Ivanov <dmi3iva@gmail.com>
2026-04-23 20:15:32 +0300

    configurate db connection

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,8 +1,10 @@
 import { MongoClient } from "mongodb";
 import { IBlogType } from "../features/blogs/models/blog.model";
 import { IPostType } from "../features/posts/models/post.model";
+import "dotenv/config";
 
 const mongoURI: string = process.env.MONGO_URI || "mongodb://0.0.0.0:27017";
+console.log("mongoURI", mongoURI);
 
 export const client = new MongoClient(mongoURI);
 
@@ -15,7 +17,7 @@ export const runDB = async () => {
   try {
     await client.connect();
 
-    await client.db("products").command({ ping: 1 });
+    await client.db("bloggersPlatform").command({ ping: 1 });
     console.log("Connected successfully to mongo server 💽");
   } catch (e) {
     console.log("Failed to connect to mongo server ⛓️‍💥");
